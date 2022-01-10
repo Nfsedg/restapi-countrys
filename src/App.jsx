6d540c4ebf463d2ac0dc1ba8c4945e02d7bdbca5
Edgar Pech <edgaropech@hotmail.com>
2022-01-09 23:21:45 -0500

    [FIX] react router for deploy without server

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Home } from './pages/Home.jsx'
 import { Details } from './pages/Details.jsx'
 import { Header } from './components/Header/index.jsx'
 import { NotFound } from './pages/NotFound.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import { SearchProvider } from './context/searchContext.js'
 import { ThemeProvider } from './context/themeContext.js'
 import './globals.css'
@@ -14,13 +14,13 @@ export const App = () => {
             <SearchProvider>
             <ThemeProvider>
                 <Header/>
-                <BrowserRouter>
+                <HashRouter>
                     <Routes>
                         <Route exact path="/" element={<Home/>}/>
                         <Route path="/detail/:countryId" element={<Details/>}/>
                         <Route element={<NotFound/>}/>
                     </Routes>
-                </BrowserRouter>
+                </HashRouter>
             </ThemeProvider>
             </SearchProvider>
         </Fragment>
